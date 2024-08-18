08f18629050077b3ec8a2c458c9342677c8c733e
SaHair529 <sa.hairulaev@gmail.com>
2024-08-18 03:13:28 +0300

    Добавил в gui возможность копирования пароля. Удаления и редактирования.

--- db_manager.py
+++ db_manager.py
@@ -1,42 +1,45 @@
+# db_manager.py
 import sqlite3
 from encryptor import Encryptor
 
 
 class DBManager:
     def __init__(self, db_file='passwords.db'):
-        self.connection = sqlite3.connect(db_file)
-        self.cursor = self.connection.cursor()
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
         self.encryptor = Encryptor()
-
         self.create_table()
 
     def create_table(self):
-        self.cursor.execute('''
-        CREATE TABLE IF NOT EXISTS passwords (
-            id INTEGER PRIMARY KEY,
-            website TEXT NOT NULL,
-            username TEXT NOT NULL,
-            password TEXT NOT NULL
-        )
-        ''')
-        self.connection.commit()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS passwords (
+                                website TEXT,
+                                username TEXT,
+                                password TEXT)''')
+        self.conn.commit()
 
     def add_password(self, website, username, password):
         encrypted_password = self.encryptor.encrypt(password)
-        self.cursor.execute('''
-        INSERT INTO passwords (website, username, password)
-        VALUES (?, ?, ?)
-        ''', (website, username, encrypted_password))
-        self.connection.commit()
+        self.cursor.execute("INSERT INTO passwords (website, username, password) VALUES (?, ?, ?)",
+                            (website, username, encrypted_password))
+        self.conn.commit()
 
-    def search_password(self, query):
+    def search_passwords(self, query):
         query = f"%{query}%"
-        self.cursor.execute('''
-        SELECT website, username, password FROM passwords
-        WHERE website LIKE ? OR username LIKE ? OR password LIKE ?
-        ''', (query, query, query))
+        self.cursor.execute("SELECT website, username, password FROM passwords WHERE website LIKE ? OR username LIKE ?",
+                            (query, query))
         results = self.cursor.fetchall()
         return [(site, user, self.encryptor.decrypt(passw)) for site, user, passw in results]
 
+    def update_password(self, old_site, old_user, new_site, new_user, new_password):
+        encrypted_password = self.encryptor.encrypt(new_password)
+        self.cursor.execute(
+            "UPDATE passwords SET website = ?, username = ?, password = ? WHERE website = ? AND username = ?",
+            (new_site, new_user, encrypted_password, old_site, old_user))
+        self.conn.commit()
+
+    def delete_password(self, site, user):
+        self.cursor.execute("DELETE FROM passwords WHERE website = ? AND username = ?", (site, user))
+        self.conn.commit()
+
     def close(self):
-        self.connection.close()
+        self.conn.close()
--- gui.py
+++ gui.py
@@ -20,8 +20,9 @@ class PasswordManagerApp:
         # Список результатов
         self.results_listbox = tk.Listbox(root, width=80, height=15)
         self.results_listbox.pack(pady=10)
+        self.results_listbox.bind("<<ListboxSelect>>", self.on_select)
 
-        # Поля для ввода новых паролей
+        # Поля для ввода/редактирования паролей
         self.website_var = tk.StringVar()
         self.username_var = tk.StringVar()
         self.password_var = tk.StringVar()
@@ -38,16 +39,40 @@ class PasswordManagerApp:
         self.password_entry = ttk.Entry(root, textvariable=self.password_var, width=50)
         self.password_entry.pack(pady=2)
 
-        # Кнопка добавления пароля
+        # Кнопки для добавления, редактирования и удаления паролей
         self.add_button = ttk.Button(root, text="Add Password", command=self.add_password)
-        self.add_button.pack(pady=10)
+        self.add_button.pack(pady=5)
+
+        self.update_button = ttk.Button(root, text="Update Password", command=self.update_password)
+        self.update_button.pack(pady=5)
+
+        self.delete_button = ttk.Button(root, text="Delete Password", command=self.delete_password)
+        self.delete_button.pack(pady=5)
+
+        # Кнопка для копирования пароля в буфер обмена
+        self.copy_button = ttk.Button(root, text="Copy Password", command=self.copy_password)
+        self.copy_button.pack(pady=5)
+
+        self.selected_record = None
 
     def update_search_results(self, *args):
         query = self.search_var.get()
-        results = self.db.search_password(query)
+        results = self.db.search_passwords(query)
         self.results_listbox.delete(0, tk.END)
         for site, user, passw in results:
             self.results_listbox.insert(tk.END, f"Website: {site}, Username: {user}, Password: {passw}")
+
+    def on_select(self, event):
+        try:
+            index = self.results_listbox.curselection()[0]
+            selected_text = self.results_listbox.get(index)
+            site, user, passw = selected_text.split(", ")
+            self.website_var.set(site.split(": ")[1])
+            self.username_var.set(user.split(": ")[1])
+            self.password_var.set(passw.split(": ")[1])
+            self.selected_record = (site.split(": ")[1], user.split(": ")[1])
+        except IndexError:
+            pass
 
     def add_password(self):
         website = self.website_var.get()
@@ -56,13 +81,52 @@ class PasswordManagerApp:
 
         if website and username and password:
             self.db.add_password(website, username, password)
-            self.website_var.set("")
-            self.username_var.set("")
-            self.password_var.set("")
+            self.clear_fields()
             messagebox.showinfo("Success", "Password added successfully!")
             self.update_search_results()
         else:
             messagebox.showwarning("Input Error", "Please fill out all fields.")
+
+    def update_password(self):
+        if self.selected_record:
+            website = self.website_var.get()
+            username = self.username_var.get()
+            password = self.password_var.get()
+
+            if website and username and password:
+                old_site, old_user = self.selected_record
+                self.db.update_password(old_site, old_user, website, username, password)
+                self.clear_fields()
+                messagebox.showinfo("Success", "Password updated successfully!")
+                self.update_search_results()
+            else:
+                messagebox.showwarning("Input Error", "Please fill out all fields.")
+        else:
+            messagebox.showwarning("Selection Error", "Please select a record to update.")
+
+    def delete_password(self):
+        if self.selected_record:
+            old_site, old_user = self.selected_record
+            self.db.delete_password(old_site, old_user)
+            self.clear_fields()
+            messagebox.showinfo("Success", "Password deleted successfully!")
+            self.update_search_results()
+        else:
+            messagebox.showwarning("Selection Error", "Please select a record to delete.")
+
+    def copy_password(self):
+        if self.selected_record:
+            self.root.clipboard_clear()
+            self.root.clipboard_append(self.password_var.get())
+            messagebox.showinfo("Success", "Password copied to clipboard!")
+        else:
+            messagebox.showwarning("Selection Error", "Please select a record to copy.")
+
+    def clear_fields(self):
+        self.website_var.set("")
+        self.username_var.set("")
+        self.password_var.set("")
+        self.selected_record = None
 
     def run(self):
         self.root.mainloop()
